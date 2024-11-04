ebeed8eac70c6917285515e82bc168b1e7c1d56a
Andrii Dotsulenko <58427426+Andrew77788@users.noreply.github.com>
2024-11-04 18:36:54 +0200

    make build

---
+++ index.f22fa8d7.js
@@ -0,0 +1,2 @@
+function t(t){document.querySelectorAll(t).forEach(function(t){var e=t.cells[1].textContent,n="THEAD"===t.parentElement.tagName||"TFOOT"===t.parentElement.tagName,o=document.createElement(n?"th":"td");o.textContent=e,t.insertBefore(o,t.cells[4])})}t("tbody tr"),t("thead tr"),t("tfoot tr");
+//# sourceMappingURL=index.f22fa8d7.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,16 @@
+"use strict";
+function addCellWithFirstColumnText(selector) {
+    const rows = document.querySelectorAll(selector);
+    rows.forEach((row)=>{
+        const firstCellText = row.cells[1].textContent;
+        const isHeaderRow = row.parentElement.tagName === "THEAD" || row.parentElement.tagName === "TFOOT";
+        const newCell = document.createElement(isHeaderRow ? "th" : "td");
+        newCell.textContent = firstCellText;
+        row.insertBefore(newCell, row.cells[4]);
+    });
+}
+addCellWithFirstColumnText("tbody tr");
+addCellWithFirstColumnText("thead tr");
+addCellWithFirstColumnText("tfoot tr");
+
+//# sourceMappingURL=index.f75de5e1.js.map
